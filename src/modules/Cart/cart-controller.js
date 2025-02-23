6f707e9d1fedb9e1efc83dd1a0fc17b30be6a74a
3b3zeem <ahmedzezo2247@gmail.com>
2025-02-23 18:36:54 +0200

    Get cart and its route

--- src/modules/Cart/cart-controller.js
+++ src/modules/Cart/cart-controller.js
@@ -1,2 +1,16 @@
 import Cart from "../../../DB/models/cart-model.js";
 
+export const getCart = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const cart = await Cart.findOne({ userId }).populate("products.productId");
+
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+
+    res.status(200).json({ cart });
+  } catch (error) {
+    next(error);
+  }
+};
