b35458d5662bc06a097908398dc509e1f158e640
sara khaled <sarahkhaled22003@gmail.com>
2025-02-26 22:05:18 +0200

    fix: remove unnecessary blank line in 404 error handler

--- src/initiate-app.js
+++ src/initiate-app.js
@@ -34,7 +34,6 @@ export const initiateApp = async (app, express) => {
   app.get("/", (req, res) => res.send("Hello World!"));
 
   app.use("*", (req, res, next) => {
-    
     res.status(404).json({ message: "Not Found" });
   });
 
