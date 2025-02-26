e75d33b72f1b5b766f73c50fb6ef6a085c8a6213
sara khaled <sarahkhaled22003@gmail.com>
2025-02-26 22:03:08 +0200

    fix: add missing line break in 404 error handler

--- src/initiate-app.js
+++ src/initiate-app.js
@@ -34,6 +34,7 @@ export const initiateApp = async (app, express) => {
   app.get("/", (req, res) => res.send("Hello World!"));
 
   app.use("*", (req, res, next) => {
+    
     res.status(404).json({ message: "Not Found" });
   });
 
