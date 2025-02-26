24cab278057d5e24607c0ed533735c416d096760
3b3zeem <ahmedzezo2247@gmail.com>
2025-02-26 18:26:44 +0200

    fix route

--- src/modules/index.routers.js
+++ src/modules/index.routers.js
@@ -3,18 +3,15 @@ import categoryRouter from "./Category/category-router.js";
 import cartRouter from "./Cart/cart-router.js";
 import productActivityRouter from "./product-activity/product-activity-router.js";
 import searchActivityRouter from "./search-activity/search-activity-router.js";
-import wishListRouter from "./WishList/WishList.router.js"
+import wishListRouter from "./WishList/WishList.router.js";
 import settingsRouter from "./settings/settings-router.js";
-
-export { authRouter, categoryRouter, cartRouter, productActivityRouter, searchActivityRouter, wishListRouter };
-
 
 export {
   authRouter,
   categoryRouter,
-  cartRouter, 
-  productActivityRouter, 
-  searchActivityRouter, 
+  cartRouter,
+  productActivityRouter,
+  searchActivityRouter,
   settingsRouter,
-  wishListRouter
+  wishListRouter,
 };
