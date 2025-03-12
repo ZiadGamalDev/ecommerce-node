0ef15a1938b5309a8522afe99d861b1c4eb8af7d
sara khaled <sarahkhaled22003@gmail.com>
2025-03-12 02:20:54 +0200

    create logs directory if it doesn't exist and update logger file path

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,16 @@
 import { createLogger, format, transports } from "winston";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
+
+const logDir = "logs";
+
+// Ensure logs directory exists
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 const silent = process.env.APP_DEBUG?.toLowerCase().trim() === "false";
 
@@ -9,9 +18,12 @@ const logger = createLogger({
   silent,
   format: format.combine(
     format.timestamp(),
-    format.printf(({ timestamp, level, message }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`)
+    format.printf(
+      ({ timestamp, level, message }) =>
+        `[${timestamp}] ${level.toUpperCase()}: ${message}`
+    )
   ),
-  transports: [new transports.File({ filename: "logs/app.log" })],
+  transports: [new transports.File({ filename: path.join(logDir, "app.log") })],
 });
 
 export default logger;
